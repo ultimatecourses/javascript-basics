545586959ce5f09c5e7bd0c63630efded725cc3f
Nishant Ingle <nishantingle5@gmail.com>
2019-10-07 18:32:06 +0530

    Added comment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import '../assets/css/style.css';
 
+// To get reference to the HTML element with id = 'app'
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
