77f9f1dec397cdab340441837e2fd3c77607b3f1
akarsh2312 <56029214+akarsh2312@users.noreply.github.com>
2020-10-01 00:35:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import '../assets/css/style.css';
 
-const app = document.getElementById('app');
+const app = document.querySelector('#app');
 app.innerHTML = '<h1><strong>JavaScript Basics</strong></h1>';
