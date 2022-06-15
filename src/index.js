987af247a6655a0353e198443d645426f36ef8ba
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2022-06-15 13:10:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-import '../assets/css/style.css';
+import "../assets/css/style.css";
 
-const app = document.getElementById('app');
-app.innerHTML = '<h1>JavaScript Basics</h1>';
+const app = document.getElementById("app");
+app.innerHTML = "<h1>JavaScript Basics</h1>";
+
+//case
+/* const number = 0;
+let result;
+
+switch (number) {
+  case 0:
+    result = "nice";
+    break;
+  case 99:
+    result = "hmm";
+    break;
+}
+console.log(result);  */
+
+//if else
+/* const number = 0;
+let result;
+
+if (number === 0) {
+  result = "nice";
+} else {
+  result = "not nice";
+}
+console.log(result);*/
