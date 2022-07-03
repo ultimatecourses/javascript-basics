0d4c544456ce4db17f50e253dd15974c52fb3f5b
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2022-07-03 13:44:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,8 +39,21 @@ console.log("Mark"[2]);*/
 const word = "Hello";
 const trans = word.toUpperCase();
 console.log(trans);*/
-/*
+
+/*ArgumentsTest
 function price() {
   console.log(arguments[1]);
 }
 price(1, 2, 3, 4);*/
+
+/*
+function pangalan(name = "Mark") {
+  console.log(`Ngalan: ${name.toUpperCase()}`);
+}
+pangalan("Mark");*/
+
+/*
+function pangalan(name) {
+  console.log(name);
+}
+pangalan("Mark");*/
